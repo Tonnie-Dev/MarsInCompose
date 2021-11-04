920e6eca8fe4d92e47e3f49fdf4f7cb6424d2074
Antony <vontonnie@gmail.com>
2021-11-04 20:09:22 +0300

    Create the Fake Interface

--- app/src/main/java/com/uxstate/marsincompose/domain/repo/EstateRepository.kt
+++ app/src/main/java/com/uxstate/marsincompose/domain/repo/EstateRepository.kt
@@ -1,7 +1,9 @@
 package com.uxstate.marsincompose.domain.repo
 
+import com.uxstate.marsincompose.data.remote.dto.EstateDTO
+
 interface EstateRepository {
 
 
-    fun getEstate()
+    fun getEstate():List<EstateDTO>
 }
