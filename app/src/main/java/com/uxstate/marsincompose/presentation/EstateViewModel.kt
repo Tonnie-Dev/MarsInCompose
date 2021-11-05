90ca873ba377b64fd87042dc826b06ade27fdf0f
Antony <vontonnie@gmail.com>
2021-11-05 20:03:07 +0300

    Write EstateViewModel

--- app/src/main/java/com/uxstate/marsincompose/presentation/EstateViewModel.kt
+++ app/src/main/java/com/uxstate/marsincompose/presentation/EstateViewModel.kt
@@ -2,9 +2,12 @@ package com.uxstate.marsincompose.presentation
 
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import com.uxstate.marsincompose.domain.model.Estate
 import com.uxstate.marsincompose.domain.use_cases.GetEstatesUseCase
 import com.uxstate.marsincompose.presentation.estatelist.EstateListState
+import com.uxstate.marsincompose.utils.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
 
 @HiltViewModel
@@ -18,9 +21,37 @@ class EstateViewModel @Inject constructor(private val useCase: GetEstatesUseCase
     private set
 
 
+    // use the injected GetEstatesUseCase Class which calls invoke implicitly
+
+    //this returns a flow of Resource Objects
+
+
+    /*we call ON-each to iterate on each element this flow emits*/
+
     private fun getEstates(){
 
-        
+     useCase().onEach {
+           result ->
+
+
+         when(result){
+
+
+             is Resource.Success -> {
+
+                 state.value = EstateListState(estates = result.data?: emptyList())
+             }
+             is Resource.Loading -> {
+
+                 state.value = EstateListState(isLoading = true)
+             }
+             is Resource.Error -> {
+
+
+                 state.value = EstateListState(error = result.message?: "An Expected Error Occurred")
+             }
+         }
+     }
 
     }
 
