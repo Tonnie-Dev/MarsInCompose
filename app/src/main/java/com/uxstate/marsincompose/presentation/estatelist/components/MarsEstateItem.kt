0f3fbb27c589dfd7e8d55dd40e92f249b87f8c0b
Antony <vontonnie@gmail.com>
2021-11-05 20:38:10 +0300

    Add MarsItem Preview fxn

---
+++ app/src/main/java/com/uxstate/marsincompose/presentation/estatelist/components/MarsEstateItem.kt
@@ -0,0 +1,15 @@
+package com.uxstate.marsincompose.presentation.estatelist.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun MarsEstateItem() {
+    
+}
+
+@Preview(name = "MarsEstateItem")
+@Composable
+fun MarsEstateItemPreview() {
+    MarsEstateItem()
+}
