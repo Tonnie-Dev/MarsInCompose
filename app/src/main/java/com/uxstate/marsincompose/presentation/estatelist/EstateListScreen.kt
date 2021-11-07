d219cce3161263961d7f46edd9cf7aedd0d1602b
Antony <vontonnie@gmail.com>
2021-11-07 18:40:57 +0300

    Set Grid to 2 Fixed Cells

--- app/src/main/java/com/uxstate/marsincompose/presentation/estatelist/EstateListScreen.kt
+++ app/src/main/java/com/uxstate/marsincompose/presentation/estatelist/EstateListScreen.kt
@@ -1,2 +1,33 @@
 package com.uxstate.marsincompose.presentation.estatelist
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyVerticalGrid
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import com.uxstate.marsincompose.presentation.EstateViewModel
+
+@ExperimentalFoundationApi
+@Composable
+fun EstateListScreen(
+    navController: NavController,
+    viewModel: EstateViewModel = hiltViewModel()
+) {
+
+
+    val state by viewModel.state
+
+
+    Box(modifier = Modifier.fillMaxSize()){
+
+        LazyVerticalGrid(cells = GridCells.Fixed(2), contentPadding = PaddingValues(5.dp)){}
+    }
+
+}
