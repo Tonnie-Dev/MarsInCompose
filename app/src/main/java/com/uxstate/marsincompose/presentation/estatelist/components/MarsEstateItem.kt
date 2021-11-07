8a19e1fb82850b0d9de3f27a82ffdff7a90ccb07
Antony <vontonnie@gmail.com>
2021-11-07 18:17:01 +0300

    Draw Icon for Sale

--- app/src/main/java/com/uxstate/marsincompose/presentation/estatelist/components/MarsEstateItem.kt
+++ app/src/main/java/com/uxstate/marsincompose/presentation/estatelist/components/MarsEstateItem.kt
@@ -4,17 +4,21 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.net.toUri
 import coil.compose.rememberImagePainter
 import com.uxstate.marsincompose.domain.model.Estate
-
+import com.uxstate.marsincompose.R
 @Composable
 fun MarsEstateItem(
-    estate: Estate, onItemClick: (Estate) -> Unit
+    estate: Estate, isLoading:Boolean, onItemClick: (Estate) -> Unit
 ) {
+
+
     val imgUri = estate.imgSrc.toUri()
         .buildUpon()
         .scheme("https")
@@ -25,11 +29,29 @@ fun MarsEstateItem(
         Image(
             painter = rememberImagePainter(
                 data = imgUri ,
-                builder = { crossfade(true) }
+                builder = { crossfade(true)
+                    placeholder(R.drawable.loading_img) }
+
             ),
             contentDescription = null,
-            modifier = Modifier.size(50.dp, 50.dp).padding(4.dp)
+            modifier = Modifier
+                .size(50.dp, 50.dp)
+                .padding(4.dp)
         )
+
+        if (estate.type=="rent"){
+            
+            Image(
+                painter = painterResource(id = R.drawable.ic_for_sale_outline),
+                contentDescription = null,
+                modifier = Modifier
+                    .padding(2.dp)
+                    .size(10.dp)
+
+                    .align(Alignment.BottomStart)
+            )
+        }
+
 
 
     }
@@ -47,6 +69,6 @@ fun MarsEstateItemPreview() {
         type = "rent"
     )
     MarsEstateItem(
-        estate = estate, onItemClick = {}
+        estate = estate, false, onItemClick = {}
     )
 }
--- app/src/main/java/com/uxstate/marsincompose/utils/Constants.kt
+++ app/src/main/java/com/uxstate/marsincompose/utils/Constants.kt
@@ -1,6 +1,8 @@
 package com.uxstate.marsincompose.utils
 
+import com.uxstate.marsincompose.R
+
 object Constants {
     const val BASE_URL = "https://android-kotlin-fun-mars-server.appspot.com/"
-
+const val XI = R.drawable.loading_img
 }
