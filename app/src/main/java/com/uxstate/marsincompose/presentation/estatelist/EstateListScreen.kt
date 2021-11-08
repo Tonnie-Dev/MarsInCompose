7faf617e2f5d74c79945abc8188d10dafba23412
Antony <vontonnie@gmail.com>
2021-11-08 18:40:24 +0300

    Add Logic for the Error State

--- app/src/main/java/com/uxstate/marsincompose/presentation/estatelist/EstateListScreen.kt
+++ app/src/main/java/com/uxstate/marsincompose/presentation/estatelist/EstateListScreen.kt
@@ -1,18 +1,24 @@
 package com.uxstate.marsincompose.presentation.estatelist
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.GridCells
 import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.uxstate.marsincompose.R
 import com.uxstate.marsincompose.presentation.EstateViewModel
 import com.uxstate.marsincompose.presentation.estatelist.components.MarsEstateItem
 
@@ -40,6 +46,20 @@ fun EstateListScreen(
 
        }
         }
+
+
+        if (state.isLoading){
+
+            CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+        }
+
+        if (state.error.isNotBlank()){
+
+            Image(painter = painterResource(id = R.drawable.ic_broken_image), contentDescription = null,
+            modifier = Modifier.size(100.dp))
+        }
     }
+
+
 
 }
