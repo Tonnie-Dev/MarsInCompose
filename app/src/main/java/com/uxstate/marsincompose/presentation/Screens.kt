aaba7e59a0109819ccf6ed9d261aff17f5091559
Antony <vontonnie@gmail.com>
2021-11-08 19:54:12 +0300

    Add Screens Enum

---
+++ app/src/main/java/com/uxstate/marsincompose/presentation/Screens.kt
@@ -0,0 +1,7 @@
+package com.uxstate.marsincompose.presentation
+
+enum class Screens(val route:String) {
+
+    OVERVIEWSCREEN("estate_overview_screen"),
+    DETAILSSCREEN("estate_details_screen")
+}
