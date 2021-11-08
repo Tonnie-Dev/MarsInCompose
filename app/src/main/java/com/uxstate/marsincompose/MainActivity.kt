1a663eaf1dec9c000c8363bd162e894a646926af
Antony <vontonnie@gmail.com>
2021-11-08 08:56:30 +0300

    Launch in ViewModelScope

--- app/src/main/java/com/uxstate/marsincompose/MainActivity.kt
+++ app/src/main/java/com/uxstate/marsincompose/MainActivity.kt
@@ -12,6 +12,10 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
 import com.uxstate.marsincompose.presentation.estatelist.EstateListScreen
 import com.uxstate.marsincompose.ui.theme.MarsInComposeTheme
+import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
+
+@AndroidEntryPoint
 @ExperimentalFoundationApi
 class MainActivity : ComponentActivity() {
 
@@ -19,6 +23,8 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+
+            Timber.i("Inside MainActivity")
             MarsInComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
--- app/src/main/java/com/uxstate/marsincompose/presentation/EstateViewModel.kt
+++ app/src/main/java/com/uxstate/marsincompose/presentation/EstateViewModel.kt
@@ -2,12 +2,15 @@ package com.uxstate.marsincompose.presentation
 
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.uxstate.marsincompose.domain.model.Estate
 import com.uxstate.marsincompose.domain.use_cases.GetEstatesUseCase
 import com.uxstate.marsincompose.presentation.estatelist.EstateListState
 import com.uxstate.marsincompose.utils.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
@@ -21,6 +24,11 @@ class EstateViewModel @Inject constructor(private val useCase: GetEstatesUseCase
     private set
 
 
+    init {
+        getEstates()
+        Timber.i("inside the initblock")
+    }
+
     // use the injected GetEstatesUseCase Class which calls invoke implicitly
 
     //this returns a flow of Resource Objects
@@ -29,7 +37,7 @@ class EstateViewModel @Inject constructor(private val useCase: GetEstatesUseCase
     /*we call ON-each to iterate on each element this flow emits*/
 
     private fun getEstates(){
-
+Timber.i("getEstates Called")
      useCase().onEach {
            result ->
 
@@ -40,18 +48,26 @@ class EstateViewModel @Inject constructor(private val useCase: GetEstatesUseCase
              is Resource.Success -> {
 
                  state.value = EstateListState(estates = result.data?: emptyList())
+
+                 Timber.i("StateIsSuccess")
+                 Timber.i("The loaded Estates are ${state.value}")
              }
              is Resource.Loading -> {
 
                  state.value = EstateListState(isLoading = true)
+                 Timber.i("StateIsLoading")
+                 Timber.i("The loaded Estates are ${state.value}")
              }
              is Resource.Error -> {
 
 
                  state.value = EstateListState(error = result.message?: "An Expected Error Occurred")
+                 Timber.i("StateIsError")
+                 Timber.i("The loaded Estates are ${state.value}")
+
              }
          }
-     }
+     }.launchIn(viewModelScope)
 
     }
 
