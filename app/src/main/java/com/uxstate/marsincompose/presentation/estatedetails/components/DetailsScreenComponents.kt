b722f7e5cd92d5ac326cdca119b8024ad2cfe356
Antony <vontonnie@gmail.com>
2021-11-10 08:34:26 +0300

    Adjust the size for the Details Screen Headers

--- app/src/main/java/com/uxstate/marsincompose/presentation/estatedetails/components/DetailsScreenComponents.kt
+++ app/src/main/java/com/uxstate/marsincompose/presentation/estatedetails/components/DetailsScreenComponents.kt
@@ -1,15 +1,14 @@
 package com.uxstate.marsincompose.presentation.estatedetails.components
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.core.net.toUri
 import coil.compose.rememberImagePainter
@@ -36,7 +35,8 @@ fun DetailImage(estate: Estate) {
         contentDescription = null,
         contentScale = ContentScale.Crop,
         modifier = Modifier
-            .fillMaxWidth().height(300.dp)
+            .fillMaxWidth()
+            .height(300.dp)
             .padding(16.dp)
 
     )
@@ -53,21 +53,31 @@ fun TypeHeaderText(estate: Estate) {
         text = if (estate.type == "rent")
             stringResource(id = R.string.type_rent)
         else
-            stringResource(id = R.string.type_sale)
+            stringResource(id = R.string.type_sale),
+
+
+        style = MaterialTheme.typography.h4,
+        modifier = Modifier.padding(paddingValues = PaddingValues(horizontal = 16.dp))
     )
 
 }
 
 @Composable
 fun PriceHeaderText(estate: Estate){
-    
-    if (estate.type=="rent"){
 
-        Text(text = stringResource(id = R.string.display_price_monthly_rental, estate.price.toDouble()))
+
+    Text(text = if (estate.type=="rent"){
+
+  stringResource(id = R.string.display_price_monthly_rental, estate.price.toDouble())
 
     }else{
-        Text(text = stringResource(id = R.string.display_price, estate.price.toDouble()))
+    stringResource(id = R.string.display_price, estate.price.toDouble())
 
-    }
+    },
+            style = MaterialTheme.typography.body1,
+        modifier = Modifier.padding(paddingValues = PaddingValues(horizontal = 16.dp, vertical = 2.dp))
+    )
+    
+
 
 }
